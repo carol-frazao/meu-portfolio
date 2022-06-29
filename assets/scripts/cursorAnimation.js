32caac833eff89d78eea31cab1050c15e1b4ca4a
Carol Frazão <94201597+carol-frazao@users.noreply.github.com>
2022-06-29 16:25:15 -0300

    criada view "Sobre mim"

--- assets/scripts/cursorAnimation.js
+++ assets/scripts/cursorAnimation.js
@@ -2,18 +2,23 @@
 
 var cursor_click = document.getElementById('cursor_click')
 var btn_start = document.getElementById('img-start')
+var arrowClick = document.getElementById('arrowStart')
 
 function clickStart() {
     click = true
 
     if(click) {
-
         cursor_click.classList.toggle('animacao_cursor');
-        btn_start.classList.toggle('scale_btn')
+        btn_start.classList.toggle('scale_btn');
+        arrowClick.style.opacity = '0';
+
+        setTimeout( function() {
+            arrowClick.style.opacity = '1'
+        }, 2500)
 
     } else {
         cursor_click.classList.remove('animacao_cursor');
-        btn_start.classList.remove('scale_btn')
+        btn_start.classList.remove('scale_btn');
     }
 }
 
