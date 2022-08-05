ada0593aa85cea2124ac97019fc93267f40741d6
Carol Frazão <94201597+carol-frazao@users.noreply.github.com>
2022-08-05 16:04:37 -0300

    adicionar botão para resetar backgroud das skills ao clicar

--- assets/scripts/eventos.js
+++ assets/scripts/eventos.js
@@ -18,7 +18,8 @@ function skillEvents() {
     const container = $('skills');
     const backgroundCover = () => container.style.backgroundSize = 'cover';
     const background = (image) => container.style.background = "url(" + image + ") center";
-    const imgSkills = document.querySelectorAll('.skill-img');
+    const btnResetBg = $('btn-reset-bg');
+    const displayResetBG = () => btnResetBg.style.opacity = "1";
     const skills = {
         html: $('html-img'),
         css: $('css-img'),
@@ -30,51 +31,75 @@ function skillEvents() {
         figma: $('figma-img')
     }
 
-    imgSkills.forEach((skill) => {
+    // set rotate on click
+    document.querySelectorAll('.skill-img').forEach((skill) => {
         skill.addEventListener("click", () => {
             setTimeout(() => skill.style.animation = "rotate-img-slide 1s", 0);
             setTimeout(() => skill.style.animation = "", 1000);
         })
     })
 
+    // set background on click
     skills.html.addEventListener("click", (e) => {
         background('assets/img/skills-wallpaper/html-wallpaper.jpg');
         backgroundCover();
+        displayResetBG();
+        btnResetBg.style.cursor = "pointer";
     })
 
     skills.css.addEventListener("click", (e) => {
         background('assets/img/skills-wallpaper/css-wallpaper.jpg');
         backgroundCover();
+        displayResetBG();
+        btnResetBg.style.cursor = "pointer"
     })
 
     skills.js.addEventListener("click", () => {
         background('assets/img/skills-wallpaper/js-wallpaper.jpg');
         backgroundCover();
+        displayResetBG();
+        btnResetBg.style.cursor = "pointer";
     })
 
     skills.react.addEventListener("click", () => {
         background('assets/img/skills-wallpaper/react-background.jpg');
         backgroundCover();
+        displayResetBG();
+        btnResetBg.style.cursor = "pointer";
     })
 
     skills.node.addEventListener("click", () => {
         background('assets/img/skills-wallpaper/node-background.png');
         backgroundCover();
+        displayResetBG();
+        btnResetBg.style.cursor = "pointer";
     })
 
     skills.git.addEventListener("click", () => {
         background('assets/img/skills-wallpaper/git-background.jpg');
         backgroundCover();
+        displayResetBG();
+        btnResetBg.style.cursor = "pointer";
     })
 
     skills.github.addEventListener("click", () => {
         background('assets/img/skills-wallpaper/github-background.jpg');
         backgroundCover();
+        displayResetBG();
+        btnResetBg.style.cursor = "pointer";
     })
 
     skills.figma.addEventListener("click", () => {
         background('assets/img/skills-wallpaper/figma-background.jpg');
         backgroundCover();
+        displayResetBG();
+        btnResetBg.style.cursor = "pointer";
+    })
+    
+    btnResetBg.addEventListener("click", () => {
+        container.style.background = "var(--bg-color-1)";
+        btnResetBg.style.opacity = "0";
+        btnResetBg.style.cursor = "default";
     })
 }
 skillEvents()
