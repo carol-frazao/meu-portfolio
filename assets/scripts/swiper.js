373511874bde00bd51f7bea6edfc0c354ba917f3
Carol Frazão <94201597+carol-frazao@users.noreply.github.com>
2022-08-25 15:52:43 -0300

    Adicionar responsividade na section Skills

--- assets/scripts/swiper.js
+++ assets/scripts/swiper.js
@@ -1,35 +1,30 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
-const swiper = new Swiper('.swiper', {
-    direction              : 'horizontal',
-    loop                   : true,
-    slidesPerView          : 4,
-    spaceBetween           : 30,
-    slidesPerGroup         : 1,
-    loopFillGroupWithBlank : true,
-    navigation             : {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10,
     },
-    breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-        1200: {
-          slidesPerView: 4,
-          spaceBetween: 30,
-        }
-    }
-})
+    670: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1000: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
 
-export default swiper
-
+export default swiper;
