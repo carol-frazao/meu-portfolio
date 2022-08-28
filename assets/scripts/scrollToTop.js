992c7f124684bde48d3341668235b0d53af012c8
Carol Frazão <94201597+carol-frazao@users.noreply.github.com>
2022-08-28 00:15:46 -0300

    Exibir btn p voltar ao topo quando scrollar pagina

---
+++ assets/scripts/scrollToTop.js
@@ -0,0 +1,13 @@
+const scrollToTop = () => window.scrollTo(0, 0);
+const posicaoInicial = (section) => $(section).position().top;
+
+$(document).scroll(function () {
+  const topo = $(document).scrollTop(); // obtem a quantidade de scroll no momento
+
+  if (posicaoInicial("#inicio") + 100 < topo) {
+    $(".back-to-top").css("display", "block");
+  } else {
+    $(".back-to-top").css("display", "none");
+    hideBtn();
+  }
+});
