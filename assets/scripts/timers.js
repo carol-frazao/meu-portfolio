cda84311a7681f7e66257dad3e0621a788d2fdc6
Carol Frazão <94201597+carol-frazao@users.noreply.github.com>
2023-03-13 14:52:10 -0300

    organizar arquivos timers

--- assets/scripts/timers.js
+++ assets/scripts/timers.js
@@ -1,6 +1,13 @@
-// Type write no título da introdução
 const text_h1 = document.getElementById("text_h1");
 
+const art_vetor =  document.getElementById("art-vetor"),
+      react_gif = document.getElementById("react_gif"),
+      css_vetor = document.getElementById("css_vetor"),
+      code_vetor = document.getElementById("code_symbol"),
+      html_vetor = document.getElementById("html"),
+      js_vetor = document.getElementById("js_vetor");
+
+// Type write no título da introdução
 function typeWhiter(text) {
   const textArray = text.innerText.split("");
   text.innerText = "";
@@ -13,32 +20,23 @@ function typeWhiter(text) {
 typeWhiter(text_h1);
 
 //Tempo para exibir os vetores
-const vetor = {
-  art_vetor: document.getElementById("art-vetor"),
-  react_gif: document.getElementById("react_gif"),
-  css_vetor: document.getElementById("css_vetor"),
-  code_vetor: document.getElementById("code_symbol"),
-  html_vetor: document.getElementById("html"),
-  js_vetor: document.getElementById("js_vetor"),
-};
-
 setTimeout(() => {
-  vetor.art_vetor.style.opacity = "1";
-  vetor.code_vetor.style.opacity = "1";
+  art_vetor.style.opacity = "1";
+  code_vetor.style.opacity = "1";
 }, 10);
 
 setTimeout(() => {
-  vetor.css_vetor.style.opacity = "1";
+  css_vetor.style.opacity = "1";
 }, 600);
 
 setTimeout(() => {
-  vetor.js_vetor.style.opacity = "1";
+  js_vetor.style.opacity = "1";
 }, 1200);
 
 setTimeout(() => {
-  vetor.react_gif.style.opacity = "1";
+  react_gif.style.opacity = "1";
 }, 1800);
 
 setTimeout(() => {
-  vetor.html_vetor.style.opacity = "1";
+  html_vetor.style.opacity = "1";
 }, 2400);
